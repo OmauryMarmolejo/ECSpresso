4e57a9978c32441e901d455b197d4ea2da4af184
Omaury Marmolejo <omaury@tango.io>
2023-12-04 16:31:51 -0600

    Format backend.tf and provider.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-ecspresso"
-    key = "production/terraform.tfstate"
-    region = "us-west-1"
+    bucket  = "terraform-state-ecspresso"
+    key     = "production/terraform.tfstate"
+    region  = "us-west-1"
     encrypt = true
-    
   }
 }
